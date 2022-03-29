b4d7d9e1440936b29e2c89c52978a9bc4462d9c1
ppoonk <101167225+ppoonk@users.noreply.github.com>
2022-03-29 17:39:34 +0800

    Update start-ponk-termux.sh

--- start-ponk-termux.sh
+++ start-ponk-termux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -e "开始安装"
-pkg install proot git python -y
-apt install wget curl tar -y
+apt install proot git python -y
+apt install wget curl tar unzip -y
 git clone https://github.com/sqlsec/termux-install-linux
 cd termux-install-linux
 
